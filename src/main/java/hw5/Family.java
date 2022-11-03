b7fc4a6494020db317ee0a3f11483ead4fe94914
Azar Ibrahimli <azer.ibrahimli.98@gmail.com>
2022-11-03 20:17:32 +0400

    Additions

--- src/main/java/hw5/Family.java
+++ src/main/java/hw5/Family.java
@@ -1,73 +1,115 @@
 package hw5;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Family {
     private Human mother;
     private Human father;
-    int size=0;
-    private Human[] children = new Human[size+1];
+    private Human[] children;
     private Pet pet;
 
 
     public Human getMother() {
         return mother;
     }
+
     public void setMother(Human mother) {
         this.mother = mother;
     }
+
     public Human getFather() {
         return father;
     }
+
     public void setFather(Human father) {
         this.father = father;
     }
+
     public Human[] getChildren() {
         return children;
     }
+
     public void setChildren(Human[] children) {
         this.children = children;
     }
+
     public Pet getPet() {
         return pet;
     }
+
     public void setPet(Pet pet) {
         this.pet = pet;
     }
 
 
-
     public Family(Human mother, Human father) {
         this.mother = mother;
         this.father = father;
-        Human[] children = new Human[0];
+        this.children = new Human[0];
+    }
+    public Family(Human mother, Human father,Pet pet) {
+        this.mother = mother;
+        this.father = father;
+        this.pet=pet;
+        this.children= new Human[0];
     }
 
 
-
-
-    public Human[] addChild(Human child){
-            children[size] = child;
-            size=size+1;
+    public Human[] addChild(Human child) {
+        Human[] ch = new Human[children.length+1];
+        for(int i=0;i<children.length;i++){
+            ch[i] = children[i];
+        }
+        ch[ch.length-1] = child;
+        children=ch;
         return children;
     }
 
+    public boolean deleteChild(int index){
+        if(children.length>index){
+            boolean delete = false;
+            Human ch[] = new Human[children.length-1];
+            for(int i=0;i<children.length;i++){
+                if(i==index){ i++;
+                    delete = true;}
+                if(delete==false){
+                    ch[i]=children[i];
+                }
+                else{
+                    ch[i-1]=children[i];
+                }
+            }
+            children = ch;
+            return true;
+        }
+        else return false;
+    }
+
+    public int countFamily(){
+        return children.length+2;
+    }
 
     @Override
     public String toString() {
-//        if(children==null){
-//            return "Family{" +
-//                    "mother=" + mother +
-//                    ", father=" + father +
-//                    ", children=" + Arrays.toString(children) +
-//                    ", pet=" + getPet() +
-//                    '}';
-//        }
-        return "Family{" +
-                "mother=" + mother +
-                ", father=" + father +
-                ", children=" + Arrays.toString(children)+
-                ", pet=" + getPet() +
-                '}';
+
+            return "Family{" +
+                    "mother=" + mother +
+                    ", father=" + father +
+                    ", children=" + Arrays.toString(children) +
+                    ", pet=" + getPet() +
+                    '}';
+        }
+
+
+    @Override
+    public boolean equals(Object given) {
+        if (given == null) return false;
+        if (given==this) return true;
+        if(!(given instanceof Family)) return false;
+        Family that = (Family) given;
+        return mother.equals(that.mother)
+                && father.equals(that.father);
     }
 }
+
--- src/main/java/hw5/Main.java
+++ src/main/java/hw5/Main.java
@@ -5,12 +5,26 @@ public class Main {
         Human mother = new Human("Malahat","Ibrahimli",1968);
         Human father = new Human("Abdurahim","Ibrahimli",1954);
         Pet pet = new Pet("Dog","Kuku",3,61,new String[]{"play,eat"});
-        Family family = new Family(mother,father);
+        Family family = new Family(mother,father,pet);
         System.out.println("Before : "+family);
-        family.addChild(new Human("Azar","Ibrahimli",1998));
+        family.addChild(new Human("Azar","Ibrahimli",1998)); //0
         System.out.println("After : "+family);
+        family.addChild(new Human("Ilgar","Ibrahimli",1998)); //1
+        System.out.println("After : "+family);
+        family.addChild(new Human("Vugar","Ibrahimli",1998)); //2
+        System.out.println("After : "+family);
+        System.out.println(family.countFamily());
+        family.deleteChild(1);
+        System.out.println("After delete : " + family);
+        System.out.println(family.deleteChild(0));
+        System.out.println("After delete : " + family);
+        System.out.println(family.countFamily());
 
+        Human mother2 = new Human("Malahat","Ibrahimli",1968);
+        Human father2 = new Human("Abdurahim","Ibrahimli",1954);
+        Pet pet2 = new Pet("Dog","Kuku",3,61,new String[]{"play,eat"});
+        Family family2 = new Family(mother2,father2,pet2);
 
+        System.out.println("Equals result: "+ family.equals(family2)); //true
     }
-
 }
